c0d32ec9684d6104df3db1de07d7b075382d293b
Chuda42 <ebmanu.r1999@gmail.com>
2023-01-30 09:39:44 -0300

    product.router get root

--- src/routes/product.router.js
+++ src/routes/product.router.js
@@ -1,15 +1,34 @@
 /* imports */
 const Router = require('express');
-const productManager = require('../manager/ProductManager');
+const ProductManager = require('../manager/ProductManager');
+
+/* const */
+const PRODUCT_PATH = './products.json';
 
 /* Router */
 const ProductRouter = Router();
 
 /* http methods */
-
 ProductRouter.get('/', async (req, res) => {
-    return res.json('Hello World');
+    try{
+        const productManager = new ProductManager(PRODUCT_PATH);
+        const productList = await productManager.getProducts();
+        
+        const { limit } = req.query ?? -1;
+
+        if (limit > 0) {
+            res.status(200).json(productList.slice(0, limit));
+        }else{
+            res.status(200).json(productList);
+        }
+    }catch(error){
+        console.log(error.message);
+    } 
 });
 
+ProductRouter.get('/:id', async (req, res) => {
+    return res.json('product by id');
+});
 
+/* export */
 module.exports = ProductRouter;
