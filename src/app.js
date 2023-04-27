52d65ef239ff2f0e597ccc3c25ed9a7b0b13ce16
Chuda42 <ebmanu.r1999@gmail.com>
2023-04-27 09:02:47 -0300

    mail route

--- src/app.js
+++ src/app.js
@@ -20,6 +20,7 @@ import cartRouter from './routers/cart.router.js';
 import chatRouter from './routers/chat.router.js';
 import sessionRouter from './routers/session.router.js';
 import smsRouter  from './routers/sms.router.js';
+import mailRouter from './routers/mail.router.js';
 import userRouter from './routers/user.router.js';
 
 /* app */
@@ -56,6 +57,7 @@ app.use('/api/carts', cartRouter);
 app.use('/api/chats', chatRouter);
 app.use('/api/sessions', sessionRouter);
 app.use('/api/sms', smsRouter);
+app.use('/api/mail', mailRouter);
 app.use('/api/users', userRouter);
 
 /* http server */
---
+++ src/controllers/mail.controller.js
@@ -0,0 +1,29 @@
+/* imports */
+import MailService from '../services/mail.service.js';
+
+/* user service */
+const mailService = new MailService();
+
+export default class MailController{
+  constructor(){
+    //setting context to this
+    Object.getOwnPropertyNames(MailController.prototype).forEach((key) => {
+      if (key !== 'constructor' && key !== 'MailService') {
+        this[key] = this[key].bind(this);
+      }
+    });
+  }
+
+  async sendMail(req, res){
+    try{
+      const { to, subject, body } = req.body;
+      const mail = await mailService.sendMail(to, subject, body);
+      res.status(201).json({ status: 'success', payload: 'Mail sent' });
+
+    }catch (error){
+      console.log(`[ERROR] ${error.message}`);
+      res.status(400).json({ status: 'error', error: error.message });
+    }
+  }
+
+}
